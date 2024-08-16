1a771a06d45ccb9bf138c81f241335fa49e7b52c
ok2da <128435545+ok2da@users.noreply.github.com>
2024-08-16 21:36:57 +0900

    [level 0] Title: 글자 지우기, Time: 0.62 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public String solution(String my_string, int[] indices) {
+        String answer = "";
+    
+        StringBuilder sb = new StringBuilder(my_string);
+        
+        Arrays.sort(indices);
+        
+        for(int i = 0; i < indices.length; i++){
+        	int index = indices[i];
+        	int length = sb.length();
+
+        	sb.deleteCharAt(index-i);        		
+        	
+        }
+        
+        answer = sb.toString();
+        
+        return answer;
+    }
+}
