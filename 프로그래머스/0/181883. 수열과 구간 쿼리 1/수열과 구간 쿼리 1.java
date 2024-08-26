2ac2176c84b202ff9df62172d1a9ab321c8c53a6
ok2da <128435545+ok2da@users.noreply.github.com>
2024-08-26 19:03:02 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 2.24 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -1,24 +1,13 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
-        int[] answer = {};
-        
-        List<Integer> list = new ArrayList<>();
-        for(int a : arr) list.add(a);
-        
+        int[] answer = Arrays.copyOfRange(arr, 0, arr.length);
+
         for(int i = 0; i < queries.length; i++){
             int s = queries[i][0];
             int e = queries[i][1];
             
-            for(int j = s; j <= e; j++){
-                list.set(j, list.get(j) + 1);
-            }
-        }
-        
-        answer = new int[list.size()];
-        
-        for(int k = 0; k < answer.length; k++){
-            answer[k] = list.get(k);
+            for(int j = s; j <= e; j++) answer[j]++;
         }
         
         return answer;
