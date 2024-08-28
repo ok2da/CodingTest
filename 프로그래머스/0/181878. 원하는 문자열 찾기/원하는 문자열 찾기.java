addd39597b1deca4ad041897c35dcb4ed92041ee
ok2da <128435545+ok2da@users.noreply.github.com>
2024-08-28 18:13:55 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 7.96 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -1,22 +1,8 @@
 class Solution {
     public int solution(String myString, String pat) {
         int answer = 0;
-        int index = 0;
         
-        for(int i = 0; i < myString.length(); i++){
-            char c_str = myString.charAt(i);
-            char c_pat = pat.charAt(index);
-            
-            if(c_str > 90) c_str = (char) (c_str - ' ');
-            if(c_pat > 90) c_pat = (char) (c_pat - ' ');
-            
-            if(c_str == c_pat) index++;
-            if(index > 0 && c_str != c_pat) index = 0;
-            if(index == pat.length()) {
-                answer = 1;
-                break;
-            }
-        }
+        if(myString.toUpperCase().indexOf(pat.toUpperCase()) != -1) answer = 1;
         
         return answer;
     }
