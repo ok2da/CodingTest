0225aa07002eb6dec585c6822501abe21edcdc1e
ok2da <128435545+ok2da@users.noreply.github.com>
2024-08-31 22:40:42 +0900

    [level 0] Title: 배열의 원소만큼 추가하기, Time: 0.36 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/0/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.java
+++ 프로그래머스/0/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.java
@@ -1,17 +1,17 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] arr) {
-        int[] answer = new int[Arrays.stream(arr).sum()];
-        int start = 0;
+        int[] answer = {};
+        int size = 0;
         
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] != 1) {
-                Arrays.fill(answer, start, start + arr[i], arr[i]);
-            }else {
-                answer[start] = arr[i];
-            }
-            start += arr[i];
-        }
+        for(int i = 0; i < arr.length; i++) size += arr[i];
+        
+        answer = new int[size];
+        int index = 0;
+        
+        for(int j = 0; j < arr.length; j++)
+            for(int k = 0; k < arr[j]; k++) answer[index++] = arr[j];
+        
         
         return answer;
     }
