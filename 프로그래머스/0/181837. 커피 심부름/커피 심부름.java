7cff80342854ed7d016707ff337770f332b1f801
ok2da <128435545+ok2da@users.noreply.github.com>
2024-09-05 22:53:54 +0900

    [level 0] Title: 커피 심부름, Time: 1.16 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -1,15 +1,14 @@
 class Solution {
     public int solution(String[] order) {
         int answer = 0;
-        for(String coffe : order){
-            if(coffe.indexOf("americano") != -1){
+        for(String coffee : order){
+            if(coffee.contains("americano") || coffee.contains("anything")){
                 answer += 4500;
-            }else if(coffe.indexOf("cafelatte") != -1){
-                answer += 5000;
             }else {
-                answer += 4500;
+                answer += 5000;
             }
         }
+        
         return answer;
     }
 }
