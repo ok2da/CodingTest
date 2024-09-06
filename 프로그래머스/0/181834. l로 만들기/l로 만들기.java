ebecc8826b39f19c93c2f18077380198910c2097
ok2da <128435545+ok2da@users.noreply.github.com>
2024-09-06 17:02:50 +0900

    [level 0] Title: l로 만들기, Time: 1144.56 ms, Memory: 380 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        
+        for(int i = 0; i < myString.length(); i++){
+            char c = myString.charAt(i);
+            answer += c < 'l' ? "l" : c;
+        }
+        
+        return answer;
+    }
+}
