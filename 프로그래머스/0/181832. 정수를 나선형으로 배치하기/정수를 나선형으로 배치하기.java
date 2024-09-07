3200cddd3aba25bf2b489978cbe8c7f501c0512f
ok2da <128435545+ok2da@users.noreply.github.com>
2024-09-07 18:36:22 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.09 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int[][] solution(int n) {
+        int[][] answer = new int[n][n];
+        int num = 1;
+        int x = 0;
+        int y = 0;
+        int min = 0;
+        int max = n - 1;
+        boolean a = false;
+        boolean b = false;
+        
+        while(num <= n*n){
+            answer[y][x] = num;
+            
+            if(x == max && y == min){
+                a = true;
+            }else if(x == max && y == max){
+                b = true;
+            }else if(x == min && y == max && a && b){
+                a = false;
+                min++;
+            }else if(x == min-1 && y == min && !a && b){
+	        	b = false;
+	        	max--;
+	        }
+            
+            if(!a && !b){
+                x++;
+                num++;
+            }else if(a && !b){
+                y++;
+                num++;
+            }else if(a && b){
+                x--;
+                num++;
+            }else if(!a && b){
+                y--;
+                num++;
+            }
+            
+        }
+        
+        return answer;
+    }
+}
