6d6c908996fdd2b53cfc5aadd3493754cf46fc94
ok2da <128435545+ok2da@users.noreply.github.com>
2024-09-14 15:16:42 +0900

    [level 0] Title: 로그인 성공?, Time: 0.03 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
@@ -1,10 +1,10 @@
 class Solution {
     public String solution(String[] id_pw, String[][] db) {
         String answer = "";
+        String id = id_pw[0];
+        String pw = id_pw[1];
         
         for(int i = 0; i < db.length; i++){
-            String id = id_pw[0];
-            String pw = id_pw[1];
             String dbid = db[i][0];
             String dbpw = db[i][1];
             
