f6ed235f911b2a31fdbc8a2152287c023b573cbb
ok2da <128435545+ok2da@users.noreply.github.com>
2024-09-17 16:53:21 +0900

    [level 0] Title: 평행, Time: 0.03 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/README.md
+++ 프로그래머스/0/120875. 평행/README.md
@@ -1,10 +1,10 @@
 # [level 0] 평행 - 120875 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120875) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120875#) 
 
 ### 성능 요약
 
-메모리: 72.1 MB, 시간: 0.04 ms
+메모리: 81.5 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 17일 16:38:39
+2024년 09월 17일 16:53:20
 
 ### 문제 설명
 
--- 프로그래머스/0/120875. 평행/평행.java
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -11,16 +11,16 @@ class Solution {
         int x4 = dots[3][0];
         int y4 = dots[3][1];
 
-        double a = (double) (x2 - x1) / (y2 - y1);
-        double b = (double) (x4 - x3) / (y4 - y3);
+        double a = (double) (x1 - x2) / (y1 - y2);
+        double b = (double) (x3 - x4) / (y3 - y4);
         if(a == b) answer = 1;
 
-        a = (double) (x3 - x1) / (y3 - y1);
+        a = (double) (x1 - x3) / (y1 - y3);
         b = (double) (x2 - x4) / (y2 - y4);
         if(a == b) answer = 1;
 
-        a = (double) (x4 - x1) / (y4 - y1);
-        b = (double) (x3 - x2) / (y3 - y2);
+        a = (double) (x1 - x4) / (y1 - y4);
+        b = (double) (x2 - x3) / (y2 - y3);
         if(a == b) answer = 1;
             
         return answer;
