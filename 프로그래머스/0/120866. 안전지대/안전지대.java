ae1a8919952348c5f3bd47b69ed990788502d7c6
ok2da <128435545+ok2da@users.noreply.github.com>
2024-09-19 00:04:21 +0900

    [level 0] Title: 안전지대, Time: 0.04 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.java
+++ 프로그래머스/0/120866. 안전지대/안전지대.java
@@ -6,12 +6,35 @@ class Solution {
         int a_max = board.length - 1;
         int b_max = board[0].length - 1;
         
+        // 위험지역을 찾아라
         for(int i = 0; i < board.length; i++){ 
             for(int j = 0; j < board.length; j++){
-                if(board[i][j] == 1) board = solution(board, i, j, min, a_max, b_max);
+                if(board[i][j] == 1) {
+                    // 기준점부터 상,하
+                    if(i - 1 >= min) board[i-1][j] = board[i-1][j] == 0 ? -1 : board[i-1][j];
+                    if(i + 1 <= a_max) board[i+1][j] = board[i+1][j] == 0 ? -1 : board[i+1][j];
+
+                    // 기준점부터 좌,우
+                    if(j - 1 >= min) board[i][j-1] = board[i][j-1] == 0 ? -1 : board[i][j-1];
+                    if(j + 1 <= b_max) board[i][j+1] = board[i][j+1] == 0 ? -1 : board[i][j+1];
+
+                    // 기준점으로 상좌, 상우
+                    if(i - 1 >= min && j - 1 >= min) 
+                        board[i-1][j-1] = board[i-1][j-1] == 0 ? -1 : board[i-1][j-1];
+                    if(i - 1 >= min && j + 1 <= b_max) 
+                        board[i-1][j+1] = board[i-1][j+1] == 0 ? -1 : board[i-1][j+1];
+
+                    // 기준점으로 하좌, 하우
+                    if(i + 1 <= a_max && j - 1 >= min) 
+                        board[i+1][j-1] = board[i+1][j-1] == 0 ? -1 : board[i+1][j-1];
+                    if(i + 1 <= a_max && j + 1 <= b_max) 
+                        board[i+1][j+1] = board[i+1][j+1] == 0 ? -1 : board[i+1][j+1];
+                }
+                
             }
         }
         
+        // 안전지역을 찾아라
         for(int i = 0; i < board.length; i++){ 
             for(int j = 0; j < board.length; j++){
                 if(board[i][j] == 0) answer++;
@@ -19,24 +42,5 @@ class Solution {
         }
         
         return answer;
-    }
-    
-    public static int[][] solution(int[][] board, int a, int b, int min, int a_max, int b_max){
-
-        if(a - 1 >= min) board[a-1][b] = board[a-1][b] == 0 ? -1 : board[a-1][b];
-        if(a + 1 <= a_max) board[a+1][b] = board[a+1][b] == 0 ? -1 : board[a+1][b];
-        
-        if(b - 1 >= min) board[a][b-1] = board[a][b-1] == 0 ? -1 : board[a][b-1];
-        if(b + 1 <= b_max) board[a][b+1] = board[a][b+1] == 0 ? -1 : board[a][b+1];
-        
-        if(a - 1 >= min && b - 1 >= min) board[a-1][b-1] = board[a-1][b-1] == 0 ? -1 : board[a-1][b-1];
-        if(a - 1 >= min && b + 1 <= b_max) board[a-1][b+1] = board[a-1][b+1] == 0 ? -1 : board[a-1][b+1];
-        
-        if(a + 1 <= a_max && b - 1 >= min) board[a+1][b-1] = board[a+1][b-1] == 0 ? -1 : board[a+1][b-1];
-        if(a + 1 <= a_max && b + 1 <= b_max) board[a+1][b+1] = board[a+1][b+1] == 0 ? -1 : board[a+1][b+1];
-        
-        
-        return board;
-
     }
 }
