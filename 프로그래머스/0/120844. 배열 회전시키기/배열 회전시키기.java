3a21bcc94d1fac454b6db655ba86b560571a1d4c
ok2da <128435545+ok2da@users.noreply.github.com>
2024-09-23 19:53:16 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.03 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.java
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.java
@@ -1,17 +1,15 @@
 import java.util.*;
 class Solution {
         public int[] solution(int[] numbers, String direction) {
-        int[] answer = new int[numbers.length];
-        int temp = 0;
-        
-        if(direction.equals("right")){
-            temp = numbers[numbers.length - 1];
-            System.arraycopy(numbers, 0, answer, 1, numbers.length - 1);
-            answer[0] = temp;
-        }else{
-            temp = numbers[0];
-            answer = Arrays.copyOfRange(numbers, 1, numbers.length + 1);
-            answer[answer.length - 1] = temp;
+        int len = numbers.length;
+        int[] answer = new int[len];
+
+        if(direction.equals("right")) {
+            answer[0] = numbers[len - 1];
+            for(int i = 1; i < len; i++) answer[i] = numbers[i-1];
+        }else {
+            answer[len - 1] = numbers[0];
+            for(int i = 0; i < len - 1; i++) answer[i] = numbers[i+1];
         }
         
         return answer;
