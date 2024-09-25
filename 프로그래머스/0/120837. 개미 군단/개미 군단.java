7ba4ddf91a300e5278340a15ae226d58d47b00d6
ok2da <128435545+ok2da@users.noreply.github.com>
2024-09-25 20:20:08 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -2,16 +2,11 @@ class Solution {
     public int solution(int hp) {
         int answer = 0;
         
-        while(hp != 0){
-            if(hp >= 5) {
-                hp -= 5;
-            }else if(hp >= 3) {
-                hp -= 3;
-            }else {
-                hp -= 1;
-            }
-            answer++;
-        }
+        answer += hp / 5;
+        hp %= 5;
+        answer += hp / 3;
+        hp %= 3;
+        answer += hp / 1;
         
         return answer;
     }
