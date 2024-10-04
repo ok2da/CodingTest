ef1e9a1d5632fd1e6c15a9665373b41ce71f006e
ok2da <128435545+ok2da@users.noreply.github.com>
2024-10-04 18:48:58 +0900

    [level 1] Title: 덧칠하기, Time: 2.02 ms, Memory: 83.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        int paint = 0;
+        
+        for(int i = 0; i < section.length; i++){
+            if(paint >= section[i]){
+                continue;
+            }else {
+                paint = section[i] + (m - 1);
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
