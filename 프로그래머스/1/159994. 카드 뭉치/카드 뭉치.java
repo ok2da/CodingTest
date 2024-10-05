a4d5b0fca4cbd220eb51a793334f3251c873d07a
ok2da <128435545+ok2da@users.noreply.github.com>
2024-10-05 12:37:14 +0900

    [level 1] Title: 카드 뭉치, Time: 0.03 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        String answer = "Yes";
+        int index1 = 0;
+        int index2 = 0;
+
+        for(int i = 0; i < goal.length; i++){
+            if(index1 <= cards1.length - 1 && cards1[index1].equals(goal[i])){
+                index1++;
+            }else if(index2 <= cards2.length - 1 && cards2[index2].equals(goal[i])){
+                index2++;
+            }else {
+                answer = "No";
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
