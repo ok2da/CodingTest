5af9350cb947956417f4371ebb9c03aa8b127a05
ok2da <128435545+ok2da@users.noreply.github.com>
2024-10-09 19:52:14 +0900

    [level 1] Title: 기사단원의 무기, Time: 145.90 ms, Memory: 94.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+
+        for(int i = 1; i <= number; i++){
+            int weapon = 0;
+
+            for(int j = 1; j <= (int) Math.sqrt(i); j++){
+                if(i % j == 0) weapon += 2;
+                if(j * j == i) weapon -= 1;
+            }
+
+            if(limit < weapon) {
+                answer += power;
+            }else {
+                answer += weapon;
+            }
+        }
+        
+        return answer;
+    }
+}
