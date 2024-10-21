818aef5f14d90560d96edcc1276f14690d679261
ok2da <128435545+ok2da@users.noreply.github.com>
2024-10-21 18:43:07 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.85 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int left, int right) {
+        int answer = 0;
+        
+        for(int i = left; i <= right; i++){
+            int number = 0;
+            for(int j = 1; j <= (int) Math.sqrt(i); j++){
+                if(i % j == 0){
+                    number++;
+                    if(j != i / j){
+                        number++;
+                    }
+                }
+            }
+            if(number % 2 == 0){
+                answer += i;
+            }else {
+                answer -= i;
+            }
+        }
+        
+        return answer;
+    }
+}
