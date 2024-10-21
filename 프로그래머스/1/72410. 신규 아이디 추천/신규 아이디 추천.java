87c4456d1bc77468a6349298fb2bc30a2eaad07d
ok2da <128435545+ok2da@users.noreply.github.com>
2024-10-22 00:00:25 +0900

    [level 1] Title: 신규 아이디 추천, Time: 13.68 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -2,31 +2,44 @@ class Solution {
     public String solution(String new_id) {
         String answer = "";
    
-        new_id = new_id.toLowerCase();
-
-        new_id = new_id.replaceAll("[^a-z0-9-_.]","");
-
-        new_id = new_id.replaceAll("\\.{2,}",".");
-
-        if(new_id.length() >= 1){
-            if(new_id.charAt(0) == '.') new_id = new_id.substring(1);
-        }
-        if(new_id.length() >= 1){
-            if(new_id.charAt(new_id.length() - 1) == '.') new_id = new_id.substring(0, new_id.length() - 1);
-        }
-
-        if(new_id.equals("")) new_id = "a";
-
-        if(new_id.length() > 15) {
-            new_id = new_id.substring(0,15);
-            if(new_id.charAt(new_id.length()- 1) == '.') new_id = new_id.substring(0, new_id.length() - 1);
-        }
-
-        if(new_id.length() == 1) new_id += new_id;
-        if(new_id.length() == 2) new_id += new_id.charAt(1);
-        
+        new_id = step_one(new_id);
+        new_id = step_two(new_id);
+        new_id = step_three(new_id);
+        new_id = step_four(new_id);
+        new_id = step_five(new_id);
+        new_id = step_six(new_id);
+        new_id = step_seven(new_id);
         answer = new_id;
         
         return answer;
     }
+    
+    public String step_one(String str){
+        return str.toLowerCase();
+    }
+    public String step_two(String str){
+        return str.replaceAll("[^a-z0-9-_.]","");
+    }
+    public String step_three(String str){
+        return str.replaceAll("\\.{2,}",".");
+    }
+    public String step_four(String str){
+        return str.replaceAll("^\\.|\\.$","");
+    }
+    public String step_five(String str){
+        return str.equals("") ? "a" : str;
+    }
+    public String step_six(String str){
+        if(str.length() >= 16) {
+            return str.substring(0,15).replaceAll("\\.$","");
+        }else {
+            return str;
+        }
+    }
+    public String step_seven(String str){
+        if(str.length() == 1) str += str.charAt(0);
+        if(str.length() == 2) str += str.charAt(1);
+        return str;
+    }
+
 }
