bf4b3ed1e8ee7e55bfdd9e854453f20848fc84d5
ok2da <128435545+ok2da@users.noreply.github.com>
2024-10-21 21:03:51 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.04 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,27 +1,23 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
-        int[] answer = {7, 7};
-        String rank = "";
+        int[] answer = {0, 0};
 
-        for(int i = 0; i < 6; i++){
-            if(lottos[i] == 0){
-                answer[0]--;
-            }else {
-                rank += "[" + lottos[i] + "]";
+        for(int l : lottos){
+                if(l == 0) {
+                    answer[0]++;
+                    continue;
+                }
+            for(int w : win_nums){
+                if(l == w){
+                    answer[0]++;
+                    answer[1]++;
+                }
             }
         }
         
-        for(int j = 0; j < 6; j++){
-            if(rank.indexOf("[" + win_nums[j] + "]") != -1){
-                answer[0]--;
-                answer[1]--;
-            }
-        }
-        
-        if(answer[0] == 7) answer[0]--;
-        if(answer[1] == 7) answer[1]--;
-
+        answer[0] = Math.min(7 - answer[0], 6);
+        answer[1] = Math.min(7 - answer[1], 6);
         
         return answer;
     }
