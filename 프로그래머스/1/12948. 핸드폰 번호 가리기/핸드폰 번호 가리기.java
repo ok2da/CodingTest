e6ea523d67ea946e04faf4dbac6bf1c54a7e8af1
ok2da <128435545+ok2da@users.noreply.github.com>
2024-11-04 18:19:49 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 18.00 ms, Memory: 91.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String phone_number) {
+        String answer = "";
+        int len = phone_number.length();
+        
+        for(int i = 0; i < len; i++){
+            if(len - i <= 4){
+                answer += phone_number.charAt(i);
+            }else {
+                answer += "*";
+            }
+        }
+        
+        return answer;
+    }
+}
