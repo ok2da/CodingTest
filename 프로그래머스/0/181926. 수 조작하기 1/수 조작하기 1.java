53831010a5fb8d662e2a9d75be8052a3e0fee6af
ok2da <128435545+ok2da@users.noreply.github.com>
2025-01-03 21:16:29 +0900

    [level 0] Title: 수 조작하기 1, Time: 7.40 ms, Memory: 93.3 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -1,22 +1,22 @@
 class Solution {
-	    public int solution(int n, String control) {
-	        int answer = 0;
-	        
-	        char[] c = control.toCharArray();
-	        
-	        for(char cc : c){
-                if(cc == 'w'){
-                    n += 1;
-                }else if(cc == 's'){
-                    n -= 1;
-                }else if(cc == 'd'){
-                    n += 10;
-                }else if(cc == 'a'){
-                    n -= 10;
-                }             
-	        }
-	        answer = n;
-            
-	        return answer;
-	    }
+    public int solution(int n, String control) {
+        int answer = 0;
+
+        char[] c = control.toCharArray();
+
+        for(char cc : c){
+            if(cc == 'w'){
+                n += 1;
+            }else if(cc == 's'){
+                n -= 1;
+            }else if(cc == 'd'){
+                n += 10;
+            }else if(cc == 'a'){
+                n -= 10;
+            }             
+        }
+        answer = n;
+
+        return answer;
+    }
 }
