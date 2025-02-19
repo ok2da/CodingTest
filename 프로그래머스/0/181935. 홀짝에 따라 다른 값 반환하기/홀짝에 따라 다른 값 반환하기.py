ad4f84f7f00ee9884c0d374b8d44ba49dffb4237
ok2da <128435545+ok2da@users.noreply.github.com>
2025-02-19 19:46:35 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.py
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.py
@@ -1,7 +1,9 @@
 def solution(n):
     answer = 0
+    # '==' 같은 비교연산자를 쓰지 않아도 0, 1에 따라 결과를 나눌 수 있음
     if n % 2 == 0:
         for i in range(2, n + 1, 2):
+            # 'i**2' 같은 거듭제곱으로 사용 가능
             answer += i * i
     else:
         for j in range(1, n + 1, 2):
