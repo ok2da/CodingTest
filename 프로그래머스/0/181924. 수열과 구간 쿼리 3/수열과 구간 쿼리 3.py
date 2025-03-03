089d05fc3a6136cf3f160516dd171021e89524d8
ok2da <128435545+ok2da@users.noreply.github.com>
2025-03-03 15:23:24 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.14 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
@@ -3,12 +3,17 @@ import copy
 def solution(arr, queries):
     answer = arr.copy()
     
-    for i in range(len(queries)):
-        a = queries[i][0]
-        b = queries[i][1]
+    # 1)
+#     for i in range(len(queries)):
+#         a = queries[i][0]
+#         b = queries[i][1]
         
-        temp = answer[a]
-        answer[a] = answer[b]
-        answer[b] = temp
+#         temp = answer[a]
+#         answer[a] = answer[b]
+#         answer[b] = temp
+
+    # 2) 다중 할당, 튜플 언패킹 -> 파이썬은 이게 된다 (임시변수 x)
+    for a, b in queries:
+        answer[a], answer[b] = answer[b], answer[a]
     
     return answer
