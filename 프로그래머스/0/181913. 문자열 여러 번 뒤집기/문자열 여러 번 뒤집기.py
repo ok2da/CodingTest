47b7fe58978b71e64eccf094f767b84d0d5d2b03
ok2da <128435545+ok2da@users.noreply.github.com>
2025-03-13 20:25:41 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 1.92 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
@@ -1,9 +1,20 @@
 
 def solution(my_string, queries):
-    answer = my_string
+    #1) O(N * M), 새로운 문자열을 지속적으로 생성
+#     answer = my_string
+    
+#     for s, e in queries:
+#         temp = answer[s : e + 1][::-1]
+#         answer = answer[0:s] + temp + answer[e + 1:]
+
+    #2) O(N + M * K), 리스트에서 슬라이싱하고 뒤집기
+    answer = ''
+    
+    char_list = list(my_string)
     
     for s, e in queries:
-        temp = answer[s : e + 1][::-1]
-        answer = answer[0:s] + temp + answer[e + 1:]
+        char_list[s : e + 1] = char_list[s : e + 1][::-1]
+        
+    answer = ''.join(char_list)
     
     return answer
