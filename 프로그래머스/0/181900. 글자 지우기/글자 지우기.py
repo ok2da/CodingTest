dd602f59f9eb58c8ba6678039a5b7bcc81fa97fb
ok2da <128435545+ok2da@users.noreply.github.com>
2025-03-23 19:29:54 +0900

    [level 0] Title: 글자 지우기, Time: 0.07 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.py
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.py
@@ -1,11 +1,8 @@
 def solution(my_string, indices):
     answer = ''
-    
-    str = list(my_string)
-    
-    for i in indices:
-        str[i] = ''
-        
-    answer = ''.join(str)
+
+    for i in range(len(my_string)):
+        if i not in indices:
+            answer += my_string[i]
     
     return answer
