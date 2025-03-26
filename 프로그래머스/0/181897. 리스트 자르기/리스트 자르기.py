0557b56e6c11bc4bb2633245eace04f0ee679e09
ok2da <128435545+ok2da@users.noreply.github.com>
2025-03-26 18:39:10 +0900

    [level 0] Title: 리스트 자르기, Time: 0.00 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.py
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.py
@@ -1,22 +1,36 @@
 def solution(n, slicer, num_list):
     answer = []
     
-    a = slicer[0]
-    b = slicer[1] + 1
-    c = slicer[2]
+    # 1)
+#     a = slicer[0]
+#     b = slicer[1] + 1
+#     c = slicer[2]
     
+#     if n == 1:
+#         for i in range(0, b):
+#             answer.append(num_list[i]) 
+#     elif n == 2:
+#         for i in range(a, len(num_list)):
+#             answer.append(num_list[i])
+#     elif n == 3:
+#         for i in range(a, b):
+#             answer.append(num_list[i])
+#     elif n == 4:
+#         for i in range(a, b, c):
+#             answer.append(num_list[i])
+    
+    # 2)
+    
+    a, b, c = slicer
+    b += 1
     
     if n == 1:
-        for i in range(0, b):
-            answer.append(num_list[i]) 
+        answer = num_list[0:b]
     elif n == 2:
-        for i in range(a, len(num_list)):
-            answer.append(num_list[i])
+        answer = num_list[a:]
     elif n == 3:
-        for i in range(a, b):
-            answer.append(num_list[i])
+        answer = num_list[a:b]
     elif n == 4:
-        for i in range(a, b, c):
-            answer.append(num_list[i])
-    
+        answer = num_list[a:b:c]
+        
     return answer
