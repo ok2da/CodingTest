dd9e32018ff0f95c7afe8d47d7729338b3ec09da
ok2da <128435545+ok2da@users.noreply.github.com>
2025-03-31 18:50:16 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 1717.98 ms, Memory: 49.3 MB -BaekjoonHub

--- 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/README.md
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/README.md
@@ -1,10 +1,10 @@
 # [level 0] 조건에 맞게 수열 변환하기 2 - 181881 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181881) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181881?language=python3) 
 
 ### 성능 요약
 
-메모리: 135 MB, 시간: 47.21 ms
+메모리: 49.3 MB, 시간: 1717.98 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 27일 19:30:43
+2025년 03월 31일 18:50:13
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.py
@@ -0,0 +1,26 @@
+def solution(arr):
+    answer = -1
+    arr_count = 0
+    
+    while(True):
+        temp_arr = []
+        result_count = 0
+        
+        for i in range(len(arr)):
+            if arr[i] >= 50 and arr[i] % 2 == 0:
+                temp_arr.append(arr[i] / 2)
+            elif arr[i] < 50 and arr[i] % 2 == 1:
+                temp_arr.append(arr[i] * 2 + 1)
+            else:
+                temp_arr.append(arr[i])
+                
+            if arr[i] == temp_arr[i]:
+                result_count += 1
+        
+        if result_count == len(arr):
+            return arr_count
+        else:
+            arr = temp_arr.copy()
+            arr_count += 1
+
+    return answer
