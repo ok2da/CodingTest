b29a56ec6d38b5f7684f1bcec5324ccb600e4f82
ok2da <128435545+ok2da@users.noreply.github.com>
2025-04-03 18:49:14 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 0.13 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.py
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.py
@@ -1,6 +1,9 @@
 def solution(myString, pat):
     
+    # 1)
     # find() = 찾고자 하는 인덱스가 첫번째 위치인 경우 0을반환, 그 외 1을반환, 찾지 못할경우 -1을 반환
-    answer = 1 if myString.lower().find(pat.lower()) >= 0 else 0
+    # answer = 1 if myString.lower().find(pat.lower()) >= 0 else 0
+    
+    answer = int(pat.lower() in myString.lower())
 
     return answer
