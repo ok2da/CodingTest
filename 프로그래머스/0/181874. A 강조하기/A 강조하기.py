b91368d8d6dd3c3a9920d8a9071ba43bfde13cef
ok2da <128435545+ok2da@users.noreply.github.com>
2025-04-05 17:26:34 +0900

    [level 0] Title: A 강조하기, Time: 0.00 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.py
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.py
@@ -1,10 +1,16 @@
 def solution(myString):
-    answer = ''
     
-    if int('a' in myString) > -1:
-        answer = myString.lower().replace('a','A')
-    else:
-        answer = myString
+#     # 1)
+#     answer = ''
+    
+#     # 멤버십 연산자 사용중 true에 해당하는 결과가 1과 0일수도 있어서 int형으로 변환 후 false인 -1과 비교
+#     if int('a' in myString) > -1:
+#         answer = myString.lower().replace('a','A')
+#     else:
+#         answer = myString
+        
+    # 2)
+    answer = myString.lower().replace('a','A')
         
     
     return answer
