8f858e1b2b07b8a43340628b5cbb361ca5f58045
ok2da <128435545+ok2da@users.noreply.github.com>
2025-04-09 19:02:38 +0900

    [level 0] Title: ad 제거하기, Time: 0.09 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/0/181870. ad 제거하기/ad 제거하기.py
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.py
@@ -1,9 +1,11 @@
 def solution(strArr):
     answer = []
+    contains = 'ad'
     
     for str in strArr:
-        # if not str.find("ad") + 1: <- 조금 이상해서 그냥 비교연산자로 수정
-        if str.find("ad") == -1:
+        # if not str.find("ad") + 1: <- 비교가 이상해서 그냥 비교연산자로 수정
+        # if str.find("ad") == -1: <- 가독성 안좋아서 수정
+        if not contains in str:
             answer.append(str)
     
     return answer
