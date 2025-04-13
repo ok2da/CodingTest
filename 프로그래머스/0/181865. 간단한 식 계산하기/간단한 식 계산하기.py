c8ab00a5e798ccad45fef4b5d46f43276ed14988
ok2da <128435545+ok2da@users.noreply.github.com>
2025-04-13 22:29:35 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.02 ms, Memory: 9.38 MB -BaekjoonHub

---
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.py
@@ -0,0 +1,11 @@
+def solution(binomial):
+    answer = binomial.split(' ')
+    
+    if answer[1] == '+':
+        return int(answer[0]) + int(answer[2])
+    elif answer[1] == '-':
+        return int(answer[0]) - int(answer[2])
+    elif answer[1] == '*':
+        return int(answer[0]) * int(answer[2])
+    
+    return 0
