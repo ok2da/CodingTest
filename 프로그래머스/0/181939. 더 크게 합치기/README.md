205897e87547966e92df03bce79bca460fb780bd
ok2da <128435545+ok2da@users.noreply.github.com>
2025-04-29 21:47:43 +0900

    [level 0] Title: 더 크게 합치기, Time: 12.90 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/README.md
+++ 프로그래머스/0/181939. 더 크게 합치기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 더 크게 합치기 - 181939 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181939?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181939?language=java) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.03 ms
+메모리: 79.4 MB, 시간: 12.90 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 16일 17:51:39
+2025년 04월 29일 21:47:41
 
 ### 문제 설명
 
--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,15 +1,12 @@
 class Solution {
-        public int solution(int a, int b) {
-      	String mix1 = String.valueOf(a) + String.valueOf(b);
-    	int gab1 = Integer.valueOf(mix1);
-      	String mix2 = String.valueOf(b) + String.valueOf(a);
-    	int gab2 = Integer.valueOf(mix2);   
-    	 int answer;
-    
-    	if ( gab1 >= gab2 ){
-            return answer = gab1;
-        } else {
-            return answer = gab2;
+    public int solution(int a, int b) {
+        String ab = "" + a + b;
+        String ba = "" + b + a;
+        
+        if (Integer.parseInt(ab) >= Integer.parseInt(ba)){
+            return Integer.parseInt(ab);
+        }else {
+            return Integer.parseInt(ba);
         }
     }
 }
