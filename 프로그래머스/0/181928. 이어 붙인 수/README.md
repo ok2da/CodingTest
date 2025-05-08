85a6660d6d6161a82e5a0c77f31fb0dcbf320b87
ok2da <128435545+ok2da@users.noreply.github.com>
2025-05-08 19:10:06 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.01 ms, Memory: 3.91 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/README.md
+++ 프로그래머스/0/181928. 이어 붙인 수/README.md
@@ -1,10 +1,10 @@
 # [level 0] 이어 붙인 수 - 181928 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181928?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181928?language=c) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.03 ms
+메모리: 3.91 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 27일 18:46:09
+2025년 05월 08일 19:10:05
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// num_list_len은 배열 num_list의 길이입니다.
+int solution(int num_list[], size_t num_list_len) {
+    int answer = 0;
+    int odd = 0;
+    int even = 0;
+    
+    for(int i = 0; i < num_list_len; i++){
+        if (num_list[i] % 2){
+            odd = odd * 10 + num_list[i];
+        }else {
+            even = even * 10 + num_list[i];
+        }
+    }
+    
+    answer = odd + even;
+    
+    return answer;
+}
