1483ef459ffc628e8eecedf5e761cf8b4b8b0fc1
ok2da <128435545+ok2da@users.noreply.github.com>
2025-05-28 18:58:03 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/README.md
+++ 프로그래머스/0/181906. 접두사인지 확인하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 접두사인지 확인하기 - 181906 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181906?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181906?language=c) 
 
 ### 성능 요약
 
-메모리: 9.1 MB, 시간: 0.00 ms
+메모리: 4.2 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 16일 21:23:13
+2025년 05월 28일 18:58:00
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+int solution(const char* my_string, const char* is_prefix) {
+    int answer = 0;
+    int idx = 0;
+    
+    while(1){
+        if(idx == strlen(is_prefix)){
+            return 1;
+        }else if(my_string[idx] == is_prefix[idx]){
+            idx++;
+        }else {
+            break;
+        }
+    }
+    
+    return answer;
+}
