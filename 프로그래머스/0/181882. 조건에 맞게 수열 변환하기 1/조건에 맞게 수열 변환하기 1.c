7337a738128637320a211ed24c2d504f8b36c39e
ok2da <128435545+ok2da@users.noreply.github.com>
2025-06-17 19:09:10 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 338.59 ms, Memory: 228 MB -BaekjoonHub

---
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int* solution(int arr[], size_t arr_len) {
+    int* answer = (int*)malloc(arr_len * sizeof(int));
+    
+    for(int i = 0; i < arr_len; i++){
+        if(arr[i] >= 50 && arr[i] % 2 == 0){
+            answer[i] = arr[i] / 2;
+        }else if(arr[i] < 50 && arr[i] % 2 != 0){
+            answer[i] = arr[i] * 2;
+        }else {
+            answer[i] = arr[i];
+        }
+    }
+    
+    return answer;
+}
