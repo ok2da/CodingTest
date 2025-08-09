2b72ba51e7d7c3128e735abaeb77ef54629f9d5f
ok2da <128435545+ok2da@users.noreply.github.com>
2025-08-09 19:50:31 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 4.06 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.c
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.c
@@ -6,7 +6,7 @@ int* solution(int array[], size_t array_len) {
     int* answer = (int*)calloc(2, sizeof(int));
     
     for(int i = 0; i < array_len; i++){
-        if(array[i] >= answer[0]){
+        if(array[i] > answer[0]){
             answer[0] = array[i];
             answer[1] = i;
         }
