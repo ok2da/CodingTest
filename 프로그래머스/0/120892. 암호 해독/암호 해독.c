738ff01f1f3671c042b73b51a4e44a61d3e37a23
ok2da <128435545+ok2da@users.noreply.github.com>
2025-08-14 16:19:03 +0900

    [level 0] Title: 암호 해독, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.c
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.c
@@ -7,10 +7,8 @@ char* solution(const char* cipher, int code) {
     int idx = 0;
     char* answer = (char*)malloc((len + 1) * sizeof(char));
     
-    for(int i = 0; i < len; i++){
-        if((i + 1) % code == 0){
-            answer[idx++] = cipher[i];
-        }
+    for(int i = code - 1; i <= len; i += code){
+        answer[idx++] = cipher[i];
     }
     
     answer[idx] = '\0';
