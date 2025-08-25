73d6a1a6c018e2a28aa3de1e5b3c9c405e3da812
ok2da <128435545+ok2da@users.noreply.github.com>
2025-08-25 15:56:38 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.27 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -2,7 +2,7 @@ def solution(my_string):
     answer = ''
     
     for ch in my_string:
-        if ch < 'a':
+        if ch < 'a': # islower()
             answer += ch.lower()
         else:
             answer += ch.upper()
