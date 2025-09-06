25f522bc56a50794d6ee6317bf0d665b3dc09834
ok2da <128435545+ok2da@users.noreply.github.com>
2025-09-06 17:41:33 +0900

    [level 0] Title: 등수 매기기, Time: 0.02 ms, Memory: 9.05 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
@@ -1,9 +1,21 @@
 def solution(score):
     answer = []
-    avgScore = [sum(s) / 2 for s in score]
-    sortScore = sorted(avgScore, reverse = True)
+    
+    # index() 사용
+#     avgScore = [sum(s) / 2 for s in score]
+#     sortedScore = sorted(avgScore, reverse = True)
 
-    for i, s in enumerate(avgScore):
-        answer.append(sortScore.index(s) + 1)
+#     for i, s in enumerate(avgScore):
+#         answer.append(sortedScore.index(s) + 1)
+
+    # 딕셔너리 사용 (범위가 늘어나면 좀더 효율적)
+    avgScore = sorted([sum(s) / 2 for s in score], reverse = True)
+    scoreRank = {}
+    
+    for i, s in enumerate(avgScore, start = 1):
+        if s not in scoreRank:  # .keys() 와 동일
+            scoreRank[s] = i
+    
+    answer = [scoreRank[sum(x) / 2] for x in score]
     
     return answer
