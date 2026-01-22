baa09bcfa6941d164f6e5c0492f27821597e7f77
ok2da <128435545+ok2da@users.noreply.github.com>
2026-01-22 19:30:19 +0900

    [level 1] Title: 공원 산책, Time: 0.55 ms, Memory: 9.39 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.py
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.py
@@ -10,8 +10,8 @@ def solution(park, routes):
     answer = find_start(W, H, park);
     
     for i in range(length):
-        op, n = find_postion(routes[i])
-        answer = move_postion(W, H, op, n, answer, park)
+        direction, distance = find_postion(routes[i])
+        answer = move_postion(W, H, direction, distance, answer, park)
         
     return answer
 
@@ -22,7 +22,7 @@ def find_start(W, H, park):
                 return [i,j]
             
 def find_postion(routes):
-    direction, distance = routes.split(' ')
+    op, n = routes.split(' ')
     
     # if direction == 'E':
     #     return [0, 1], int(distance)
@@ -35,18 +35,18 @@ def find_postion(routes):
     # else:
     #     return 'ERROR'
     
-    if direction not in MOVE:
+    if op not in MOVE:
         return 'ERROR'
     
-    return list(MOVE[direction]), int(distance)
+    return list(MOVE[op]), int(n)
     
             
-def move_postion(W, H, op, n, answer, park):
+def move_postion(W, H, direction, distance, answer, park):
     temp = answer[:] # copy()
     
-    for _ in range(n):
-        temp[0] += op[0]
-        temp[1] += op[1]
+    for _ in range(distance):
+        temp[0] += direction[0]
+        temp[1] += direction[1]
         
         if temp[0] < 0 or temp[0] >= H or temp[1] < 0 or temp[1] >= W or park[temp[0]][temp[1]] == 'X':
             return answer
