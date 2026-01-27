5761607e27b3ead76f9d1239a03f85e5c61638a8
ok2da <128435545+ok2da@users.noreply.github.com>
2026-01-27 18:23:22 +0900

    [level 1] Title: 콜라 문제, Time: 0.03 ms, Memory: 4.38 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int a, int b, int n) {
+    int answer = 0;
+    
+    while(n >= a) {
+        answer += n / a * b;
+        n = ((n / a) * b) + (n % a);
+    }
+    
+    return answer;
+}
